1c6d1e4138c920d55881249c4bbf1fc3edf8743f
HB-user <HB-user@IKSCEWKS02618>
2023-04-12 05:54:59 +0800

    auth done

--- app/src/main/java/libratech/auth/login.java
+++ app/src/main/java/libratech/auth/login.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.ValueEventListener;
-import dashboard.home;
+import libratech.dashboard.home;
 import java.awt.Font;
 import libratech.models.auth;
 import java.io.FileNotFoundException;
