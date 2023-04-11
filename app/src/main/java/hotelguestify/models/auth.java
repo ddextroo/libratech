da759da9d9db4f1c430b6dea939d19ac73f9b26e
HB-user <HB-user@IKSCEWKS02618>
2023-04-11 14:32:40 +0800

    error pa

--- app/src/main/java/hotelguestify/models/auth.java
+++ app/src/main/java/hotelguestify/models/auth.java
@@ -1,65 +1,61 @@
 package hotelguestify.models;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import hotelguestify.util.firebaseInit;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.common.collect.ImmutableList;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.UserRecord;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import okhttp3.*;
-import java.io.FileInputStream;
 import java.io.IOException;
 import javax.swing.JOptionPane;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class auth {
+public class auth{
 
-    private String accessToken;
-    private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
-    private DatabaseReference user = _firebase.getReference("user");
-    private ChildEventListener _user_child_listener;
-    private FirebaseAuth auth = FirebaseAuth.getInstance();
-    private UserRecord userRecord;
+    private FirebaseAuth firebaseAuth;
+    private String email;
+    private String password;
+    private DatabaseReference databaseReference;
+    private HashMap<String, Object> m;
+    private pushValue v;
     
-    public auth(String email, String password) throws IOException {
-        new firebaseInit().initFirebase();
 
-        OkHttpClient client = new OkHttpClient();
-
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\n    \"email\": \"" + email + "\",\n    \"password\": \"" + password + "\",\n    \"returnSecureToken\": true\n}");
-        Request request = new Request.Builder()
-                .url("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAWrYbhD7Kb7YtbifRSTvVKzh7ZudWLMGE")
-                .post(body)
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .addHeader("Content-Type", "application/json")
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        if (response.isSuccessful()) {
-            JsonObject jsonResponse = new Gson().fromJson(response.body().string(), JsonObject.class);
-            String idToken = jsonResponse.get("idToken").getAsString();
-            JOptionPane.showMessageDialog(null, "Login successfully " + idToken);
-            userRecord = FirebaseAuth.getInstance().getUser(uid);
-            if (user != null) {
-                String uid = useruid.getUid();
-                System.out.println("The user's UID is: " + uid);
-            } else {
-                // No user is signed in
-            }
-            // do something with the idToken
-        } else {
-            // handle the error
-            int errorCode = response.code();
-            String errorMessage = response.body().string();
-            JsonObject errorJson = new Gson().fromJson(errorMessage, JsonObject.class);
-            String messageKey = errorJson.get("error").getAsJsonObject().get("message").getAsString();
-            JOptionPane.showMessageDialog(null, "Login failed with error code " + messageKey);
-        }
+    public auth(String email, String password) throws FileNotFoundException {
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.databaseReference = FirebaseDatabase.getInstance().getReference();
+        this.email = email;
+        this.password = password;
     }
+
+    public void signUp() throws FirebaseAuthException {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setEmail(email)
+                .setPassword(password);
+        UserRecord userRecord = firebaseAuth.createUser(request);
+        System.out.println("Successfully created user: " + userRecord.getUid());
+        v = new pushValue(userRecord.getUid());
+        m = new HashMap<>();
+        m.put("email", email);
+        m.put("pass", password);
+        v.pushData("users", m);
+    }
+
+    public String login() throws FirebaseAuthException {
+        UserRecord userRecord = firebaseAuth.getUserByEmail(email);
+        String uid = firebaseAuth.createCustomToken(userRecord.getUid());
+        return uid;
+    }
+
+//    public void forgotPassword(String email) throws FirebaseAuthException {
+//        firebaseAuth.sendPasswordResetEmail(email);
+//        System.out.println("Password reset email sent to: " + email);
+//    }
 }
